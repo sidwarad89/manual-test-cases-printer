d4a34afd0caa8716602ace60b7a0e6e4f49fc03d
sidwarad89 <sidwarad89@gmail.com>
2026-09-10 09:16:16 +0530

    Add generated automation script: src/main/python/pages/login_page.py

--- src/main/python/pages/login_page.py
+++ src/main/python/pages/login_page.py
@@ -1,19 +1,21 @@
+"""Login page object model."""
 from selenium.webdriver.common.by import By
 from .base_page import BasePage
 
-class LoginPage(BasePage):
-    """Page object representing the login screen."""
 
-    # Locators
+class LoginPage(BasePage):
     USERNAME_INPUT = (By.ID, "username")
     PASSWORD_INPUT = (By.ID, "password")
-    SUBMIT_BUTTON  = (By.CSS_SELECTOR, "button[type='submit']")
+    SUBMIT_BUTTON = (By.CSS_SELECTOR, "button[type='submit']")
+    ERROR_MESSAGE = (By.CSS_SELECTOR, ".error")
 
     def __init__(self, driver):
         super().__init__(driver)
 
     def login(self, username: str, password: str):
-        """Perform login with provided credentials."""
         self.type(self.USERNAME_INPUT, username)
         self.type(self.PASSWORD_INPUT, password)
         self.click(self.SUBMIT_BUTTON)
+
+    def get_error_message(self):
+        return self.find(self.ERROR_MESSAGE).text
