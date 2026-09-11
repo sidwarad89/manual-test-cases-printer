b4dbcdbf0a7519ce057a9aefadd6d9237e09ff09
sidwarad89 <sidwarad89@gmail.com>
2026-09-11 10:10:34 +0530

    Add generated automation script: src/pages/base_page.py

--- src/pages/base_page.py
+++ src/pages/base_page.py
@@ -1,27 +1,32 @@
+from appium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.common.exceptions import NoSuchElementException, TimeoutException
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 
+
 class BasePage:
-    """All page objects inherit from this class."""
-
-    def __init__(self, driver):
+    def __init__(self, driver: webdriver.Remote):
         self.driver = driver
-        self.wait = WebDriverWait(driver, 10)
+        self.wait = WebDriverWait(self.driver, 20)
 
-    def open(self, url):
-        self.driver.get(url)
+    def find(self, locator: tuple):
+        return self.wait.until(EC.presence_of_element_located(locator))
 
-    def find(self, locator):
-        return self.wait.until(EC.visibility_of_element_located(locator))
+    def click(self, locator: tuple):
+        element = self.find(locator)
+        element.click()
 
-    def click(self, locator):
-        elem = self.find(locator)
-        elem.click()
+    def send_keys(self, locator: tuple, text: str):
+        element = self.find(locator)
+        element.clear()
+        element.send_keys(text)
 
-    def type(self, locator, text):
-        elem = self.find(locator)
-        elem.clear()
-        elem.send_keys(text)
+    def is_displayed(self, locator: tuple) -> bool:
+        try:
+            return self.find(locator).is_displayed()
+        except (NoSuchElementException, TimeoutException):
+            return False
 
-    def get_text(self, locator):
+    def get_text(self, locator: tuple) -> str:
         return self.find(locator).text
